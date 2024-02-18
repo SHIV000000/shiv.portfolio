52bffd6b7f7149c42b8cb079297361c1fd471e05
SHIVAM RISHINARAYAN SINGH <109292385+SHIV000000@users.noreply.github.com>
2024-02-18 15:59:25 +0530

    new commit for my portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Toggle mobile navigation menu
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
